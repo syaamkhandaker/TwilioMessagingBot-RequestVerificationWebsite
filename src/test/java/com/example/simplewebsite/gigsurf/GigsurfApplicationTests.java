5434768bd49edb20e1cd3986cdd16f6e758b3ec4
Syaam Khandaker <70422626+syaamkhandaker@users.noreply.github.com>
2022-11-16 19:11:19 -0500

    Add files via upload

---
+++ src/test/java/com/example/simplewebsite/gigsurf/GigsurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewebsite.gigsurf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigsurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
