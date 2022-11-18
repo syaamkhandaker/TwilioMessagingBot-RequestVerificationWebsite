f1d5b2fdce7527c18cb1b9ecda87dfa54b92ef02
Syaam Khandaker <70422626+syaamkhandaker@users.noreply.github.com>
2022-11-18 12:01:48 -0500

    Delete src/main/java/com/example/example/simplewebsite/gigsurf directory

--- src/main/java/com/example/example/simplewebsite/gigsurf/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simplewebsite.gigsurf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * RequestRepository
- */
-public interface RequestRepository extends JpaRepository<Request,Integer>{
-}
